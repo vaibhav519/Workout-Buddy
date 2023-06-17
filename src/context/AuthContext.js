7dced32ddd44a7991bf4333f75d7b88879f704dd
vaibhav519 <vaibhav3raj@gmail.com>
2023-06-17 18:41:04 +0530

    till useLogout hook

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -17,7 +17,7 @@ export const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, { user: null });
-
+  console.log(state)
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
